9dc84bb3faf3319ac39b433c29c22e431349099b
Malkeet Singh <malkeet.singh@lendi.com.au>
2022-09-22 15:15:53 +1000

    initilise app with nodejs-aws-typescript template

---
+++ serverless.ts
@@ -0,0 +1,33 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'reminder-app',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    profile: "link2malkeet",
+    region: "ap-southeast-2",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
